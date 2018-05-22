fd3f32cbe71ab12c915e5d4a26c31aefb1e581d9
dorianinc <dorianrdba@gmail.com>
2018-05-22 13:06:27 -0700

    .

---
+++ data/friends.js
@@ -0,0 +1,26 @@
+// ===============================================================================
+// DATA
+// Below data will hold all the results from the survey.
+// ===============================================================================
+
+var friendsArray = {
+    "name":"Orisa",
+    "photo":"http://ksassets.timeincuk.net/wp/uploads/sites/54/2017/03/orisa-1.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  }
+  
+
+
+// Export the array. This makes it accessible to other files using require.
+module.exports = friendsArray;
---
+++ server.js
@@ -0,0 +1,23 @@
+// Dependencies
+var express = require('express');
+var bodyParser = require("body-parser");
+
+// Create an instance of the express app.
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+// Router
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+// Listener
+app.listen(PORT, function(){
+    console.log('App Listening on PORT: ' + PORT);
+});
